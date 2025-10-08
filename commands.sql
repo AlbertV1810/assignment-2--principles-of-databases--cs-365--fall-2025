b7acc9610aaa5c69d46b68a1f35cf31e21cd3608
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-10-08 19:58:46 -0400

    fixed variable naming

--- commands.sql
+++ commands.sql
@@ -2,22 +2,22 @@
 INSERT INTO passwords (user_id, website_name, url, password, comment)
 VALUES (
   1,
-  'Zoom',
-  'https://zoom.us',
-  AES_ENCRYPT('zoomSecure11', 'secretKey'),
-  'Video conferencing account'
+  '[WEBSITE_NAME]',
+  '[URL]',
+  AES_ENCRYPT('[PLAINTEXT_PASSWORD]', 'secret_key'),
+  '[COMMENT]'
 );
 
 -- 2. Retrieve decrypted password for a specific URL
-SELECT CAST(AES_DECRYPT(password, 'secretKey') AS CHAR) AS decrypted_password
+SELECT CAST(AES_DECRYPT(password, 'secret_key') AS CHAR) AS decrypted_password
 FROM passwords
-WHERE url = 'https://mysql.com';
+WHERE url = '[TARGET_URL]';
 
 -- 3. Retrieve full data including decrypted password for https URLs
 SELECT
   p.website_name,
   p.url,
-  CAST(AES_DECRYPT(p.password, 'secretKey') AS CHAR) AS decrypted_password,
+  CAST(AES_DECRYPT(p.password, 'secret_key') AS CHAR) AS decrypted_password,
   p.comment,
   p.created_at,
   u.first_name,
@@ -30,18 +30,18 @@ WHERE p.url LIKE 'https%';
 
 -- 4. Update a URL from http to https
 UPDATE passwords
-SET url = 'https://netflix.com'
-WHERE website_name = 'Netflix';
+SET url = '[NEW_URL]'
+WHERE website_name = '[TARGET_SITE]';
 
 -- 5. Update password for a specific site
 UPDATE passwords
-SET password = AES_ENCRYPT('newNetflixPass', 'secretKey')
-WHERE website_name = 'Netflix';
+SET password = AES_ENCRYPT('[NEW_PASSWORD]', 'secret_key')
+WHERE website_name = '[TARGET_SITE]';
 
 -- 6. Delete password entry by URL
 DELETE FROM passwords
-WHERE url = 'http://twitter.com';
+WHERE url = '[TARGET_URL]';
 
--- 7. Delete password entry by encrypted password value
+-- 7. Delete password entry by password value
 DELETE FROM passwords
-WHERE password = AES_ENCRYPT('streamPass4', 'secretKey');
+WHERE password = AES_ENCRYPT('PLAINTEXT_PASSWORD', 'secret_key');
